559990633a6a08130b3fbde7ae732807501507c7
Abhishek Kumar <abhi993575@gmail.com>
2024-06-28 12:03:14 +0530

    adding files

--- first-app/src/screens/ToDoScreens.js
+++ first-app/src/screens/ToDoScreens.js
@@ -1,10 +1,26 @@
 import { Component } from "react";
 
 class TodoScreen extends Component{
-
+    state={
+        taskCount: 0,
+    }
+    
     render(){
         return(
-            <h1 className="ui heading center">To Do List</h1>
+            <div className="screen">
+                <h1 className="ui heading center">To Do List</h1>
+                <div>
+                    <button 
+                    onClick={(e)=>{
+                        this.setState({
+                            ...this.state, 
+                            taskCount: this.state.taskCount+1
+                        })
+                        console.log(this.state.taskCount);
+                    }} className="ui secondary button">Add Task</button>
+                    <p>The count of button was clicked: {this.state.taskCount}</p>
+                </div>
+            </div>
         )
     }
 }
