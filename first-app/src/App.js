8c9d696eb93e1b0f9375679523d0d445998ca94c
Abhishek Kumar <abhi993575@gmail.com>
2024-06-28 12:40:49 +0530

    adding files

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,14 @@
 import { Component } from "react";
 import TodoScreen from "./screens/ToDoScreens";
 
-class App extends Component{
-    render(){
-        return <TodoScreen/>
-    }
-}
+// class App extends Component{
+//     render(){
+//         return <TodoScreen/>
+//     }
+// }
+
+const App = () => {
+    return <TodoScreen/>
+};
 
 export default App;
--- first-app/src/screens/ToDoScreens.js
+++ first-app/src/screens/ToDoScreens.js
@@ -1,28 +1,25 @@
-import { Component } from "react";
+import { useState } from "react";
+import Task from "../components/Task";
 
-class TodoScreen extends Component{
-    state={
-        taskCount: 0,
-    }
-    
-    render(){
-        return(
+
+const TodoScreen =() => {
+    const [taskList, setTaskList]=useState([]);
+    return(
             <div className="screen">
                 <h1 className="ui heading center">To Do List</h1>
                 <div>
                     <button 
-                    onClick={(e)=>{
-                        this.setState({
-                            ...this.state, 
-                            taskCount: this.state.taskCount+1
-                        })
-                        console.log(this.state.taskCount);
-                    }} className="ui secondary button">Add Task</button>
-                    <p>The count of button was clicked: {this.state.taskCount}</p>
+                        onClick={(e)=>{
+                            setTaskList([...taskList, {
+                                title: "Go to gym",
+                                discription: "Going to gym is good for muscle growth."
+                            }])
+                        }}
+                     className="ui secondary button">Add Task</button>
+                {taskList.map((task) => <Task/>)}
                 </div>
             </div>
         )
-    }
 }
 
 export default TodoScreen;
