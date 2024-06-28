39643f4b9718f5aade0f33452e7097d456fdf265
Abhishek Kumar <abhi993575@gmail.com>
2024-06-28 11:29:35 +0530

    adding files

---
+++ first-app/src/App.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import TodoScreen from "./screens/ToDoScreens";
+
+class App extends Component{
+    render(){
+        return <TodoScreen/>
+    }
+}
+
+export default App;
